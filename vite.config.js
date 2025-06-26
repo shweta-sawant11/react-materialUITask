aec87fa02dd7c1d3582f50c37337b29f02a61f39
shweta-sawant11 <shwetasawant1499@gmail.com>
2025-06-26 22:29:19 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Use this only if deploying to GitHub Pages:
+const repoName = 'react-materialUITask' // 👈 change this to match your GitHub repo
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
